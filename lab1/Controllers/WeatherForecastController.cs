04f348b060c30d8d7b11a7573a4b2ae9582b1983
ryumixx <turbaevv@internet.ru>
2023-10-07 10:42:14 +0300

    lab1

---
+++ lab1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        public WeatherForecastController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Вот информационное сообщение от нашего контроллеразначений.");
+           
+            _logger.LogDebug("Вот отладочное сообщение от нашего контроллеразначений.");
+           
+            _logger.LogWarn("Вот сообщение предупреждения от нашего контроллеразначений.");
+           
+            _logger.LogError("Вот сообщение об ошибке от нашего контроллеразначений.");
+        return new string[] { "value1", "value2" };
+        }
+    }
+}
